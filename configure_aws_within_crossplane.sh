87458b4544a60eaf620f3ad9c1c81c0b560923d6
arshukla98 <153285001+arshukla98@users.noreply.github.com>
2024-04-08 10:24:15 +0530

    Create configure_aws_within_crossplane.sh

---
+++ configure_aws_within_crossplane.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Create Crossplane Provider
+cat <<EOF | kubectl apply -f -
+apiVersion: pkg.crossplane.io/v1
+kind: Provider
+metadata:
+  name: crossplane-provider-aws-1
+spec:
+  package: crossplane-contrib/provider-aws:v0.47.1
+EOF
+
+echo "Crossplane Provider created."
+
+# Wait for the Crossplane Provider to be installed
+sudo microk8s kubectl wait --for=condition=Healthy provider.pkg.crossplane.io/crossplane-provider-aws-1
+
+# Get the Crossplane Provider
+sudo microk8s kubectl get provider.pkg.crossplane.io
+
+# Create Provider Config
+cat <<EOF | kubectl apply -f -
+apiVersion: aws.crossplane.io/v1beta1
+kind: ProviderConfig
+metadata:
+  name: crossplane-provider-config
+spec:
+  credentials:
+    source: Secret
+    secretRef:
+      namespace: crossplane-system
+      name: aws-secret
+      key: creds
+EOF
+
+echo "Provider Config created."
+
+# Wait for the Provider Config to be created
+sudo microk8s kubectl wait --for=condition=Ready providerconfig.aws.crossplane.io/crossplane-provider-config
+
+# Get the ProviderConfigs
+sudo microk8s kubectl get providerconfig.aws.crossplane.io
