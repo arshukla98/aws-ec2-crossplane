5441f6b2627719a79d9e99762f52d1670fd822da
arshukla98 <153285001+arshukla98@users.noreply.github.com>
2024-04-17 10:31:12 +0530

    Removing resourceName

--- create_ec2_instance_v1.sh
+++ create_ec2_instance_v1.sh
@@ -22,8 +22,8 @@ EOF
 }
 
 # Check if all required arguments are provided
-if [ "$#" -ne 6 ]; then
-    echo "Usage: $0 <imageID> <instanceType> <region> <keyName> <vmName> <resourceName>"
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 <imageID> <instanceType> <region> <keyName> <vmName>"
     exit 1
 fi
 
@@ -32,8 +32,9 @@ imageID=$1
 instanceType=$2
 region=$3
 keyName=$4
-vmName=$5
-resourceName=$6
+vmName="$5"
+resourceName="backstage-ec2-$(cat /proc/sys/kernel/random/uuid | cut -d '-' -f1)"
+vmName="$vmName-$(cat /proc/sys/kernel/random/uuid | cut -d '-' -f1)"
 
 # Call function to generate YAML
 generate_yaml > ec2-instance.yaml
@@ -46,3 +47,9 @@ sudo microk8s kubectl create -f ec2-instance.yaml
 sudo microk8s kubectl wait --timeout=5m --for=condition=Ready instance.ec2.aws.crossplane.io/$resourceName
 
 sudo microk8s kubectl get instances.ec2.aws.crossplane.io -A
+
+sudo mkdir -p artifacts
+
+sudo microk8s kubectl describe instances.ec2.aws.crossplane.io $resourceName > artifacts/instanceInfo.txt
+
+echo 'Instance info is uploaded to action artifact.'
