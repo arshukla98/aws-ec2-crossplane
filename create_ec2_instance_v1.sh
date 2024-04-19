937ac31ff8d3a825a8f4be20f9587432803b6917
arshukla98 <arshukla@msystechnologies.com>
2024-04-19 11:42:26 +0530

    fix

--- create_ec2_instance_v1.sh
+++ create_ec2_instance_v1.sh
@@ -7,6 +7,8 @@ apiVersion: ec2.aws.crossplane.io/v1alpha1
 kind: Instance
 metadata:
   name: $resourceName
+  labels:
+    instanceName: $vmName
 spec:
   forProvider:
     region: $region
@@ -15,7 +17,7 @@ spec:
     keyName: $keyName
     tags:
       - key: Name
-        value: "$vmName"
+        value: $vmName
   providerConfigRef:
     name: crossplane-provider-config
 EOF
@@ -32,9 +34,8 @@ imageID=$1
 instanceType=$2
 region=$3
 keyName=$4
-vmName="$5"
+vmName=$5
 resourceName="backstage-ec2-$(cat /proc/sys/kernel/random/uuid | cut -d '-' -f1)"
-vmName="$vmName-$(cat /proc/sys/kernel/random/uuid | cut -d '-' -f1)"
 
 # Call function to generate YAML
 generate_yaml > ec2-instance.yaml
@@ -46,10 +47,15 @@ sudo microk8s kubectl create -f ec2-instance.yaml
 # Wait for the EC2 Instance to be ready
 sudo microk8s kubectl wait --timeout=5m --for=condition=Ready instance.ec2.aws.crossplane.io/$resourceName
 
-sudo microk8s kubectl get instances.ec2.aws.crossplane.io -A
+# sudo microk8s kubectl get instances.ec2.aws.crossplane.io -A
 
 sudo mkdir -p artifacts
 
-sudo microk8s kubectl describe instances.ec2.aws.crossplane.io $resourceName > artifacts/instanceInfo.txt
+# cd artifacts && sudo touch instanceInfo.txt
 
-echo 'Instance info is uploaded to action artifact.'
+sudo microk8s kubectl describe instances.ec2.aws.crossplane.io -l "instanceName=$vmName" | sudo bash -c 'cat > artifacts/instanceInfo.txt'
+# sudo microk8s kubectl get instances -o=jsonpath='{range .items[?(@.spec.forProvider.tags[0].value==<instanceName>)]}{.metadata.name}{"\n"}{.spec.forProvider.imageId}{"\n"}{.spec.forProvider.instanceType}{"\n"}{.spec.forProvider.region}{"\n"}{.status.atProvider.state}{"\n"}{.status.atProvider.publicDnsName}{"\n"}{end}'
+
+ls -la artifacts
+
+echo 'instance information is saved in a text file.'
